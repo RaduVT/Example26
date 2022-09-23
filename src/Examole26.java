2f338e912c1ea01dc8886804efdb1c17373d5583
rvt <alutoa2411@ieselcaminas.org>
2022-09-23 19:06:36 +0200

    Initial commit

---
+++ src/Examole26.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Examole26 {
+    public static void main(String[] args){
+        int num, div, i;
+
+        Scanner inpuValue;
+        inpuValue = new Scanner(System.in);
+
+        System.out.println("Enter the number:");
+        num = inpuValue.nextInt();
+
+
+        for (i = 1; i <= num; i++){
+            if (num % i == 0){
+                System.out.println(i);
+            }
+        }
+    }
+}
+
